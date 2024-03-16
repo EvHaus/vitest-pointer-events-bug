a0944df96a2a3a8fc64c46d9887528b059b635d9
Ev Haus <ev@haus.gg>
2024-03-15 23:40:31 -0700

    Try with flat hierarchy

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     test: {
-        css: {
-            modules: {
-                classNameStrategy: 'non-scoped'
-            }
-        },
         environment: 'happy-dom',
         setupFiles: ['setupFile.ts'],
         watch: false
