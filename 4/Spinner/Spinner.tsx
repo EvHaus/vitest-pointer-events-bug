e38fb50c3c7969cd45bfe8585d82bebda42b5432
Ev Haus <ev@haus.gg>
2024-03-15 23:40:55 -0700

    Put back 5 replicas

---
+++ 4/Spinner/Spinner.tsx
@@ -0,0 +1,46 @@
+import clsx from 'clsx';
+import React from 'react';
+
+const styles = {};
+
+type PropsType = {
+	className?: string | null,
+	color?: 'default' | 'white',
+	size?: number,
+}
+
+const Spinner = ({
+	className,
+	color = 'default',
+	size = 16,
+}: PropsType) => {
+	const style = {
+		height: size,
+		width: size,
+	};
+
+	const classes = clsx(
+		styles.main,
+		styles[color],
+		className
+	);
+
+	return (
+		<div className={classes} style={style}>
+			<svg
+				className={styles.svg}
+				role='img'
+				viewBox='0 0 150 150'
+				x='0'
+				y='0'>
+				<circle
+					className={styles.circle}
+					cx='75'
+					cy='75'
+					r='60' />
+			</svg>
+		</div>
+	);
+};
+
+export default Spinner;
